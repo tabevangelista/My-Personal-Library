5dae7c404da6b30a1c2f59ded712af792782df15
tabevangelista <165334183+tabevangelista@users.noreply.github.com>
2024-03-28 18:05:48 -0400

    Add files via upload

---
+++ mylibrary.py
@@ -0,0 +1,37 @@
+print("Welcome to Etabs") 
+print("This is your personal library made by yours truly") 
+TBRList = {}
+RList = {}
+FinList = {} 
+DNFList = {}
+
+Rbook1 = input ("What book are you reading right now? ")
+Rauthor1 = input("Who's the author? ")
+RList.update({Rbook1 : Rauthor1})
+
+TBRbook1 = input("What book do you want to read? ")
+TBRauthor1 = input("Whos the author? ")
+TBRList.update({TBRbook1 : TBRauthor1})
+
+print(RList) 
+print(TBRList)  
+
+
+
+
+
+    
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
